997c3ffd5357188369db12e1617813aa96edc60b
Vineeth Reddy B <vineethreddyys@gmail.com>
2024-01-31 17:50:13 +0530

    h2databaseconnection

---
+++ src/main/java/com/example/h2databaseexample/H2databaseexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.h2databaseexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2databaseexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2databaseexampleApplication.class, args);
+	}
+
+}
